b39bac5c0eeeb2bb293eefd1b1f5812fac5e219c
Andreas Schjønhaug <andreas@schjonhaug.com>
2019-03-26 10:45:30 +0100

    Single migration

--- functions/src/migrate/index.ts
+++ functions/src/migrate/index.ts
@@ -3,6 +3,37 @@ import * as functions from "firebase-functions"
 import database from "../database"
 
 async function migrate(request: functions.Request, response: functions.Response) {
+  const transcriptId = request.query.transcriptId
+
+  const transcript = await database.getTranscript(transcriptId)
+
+  const status = transcript.process || undefined
+  if (status !== undefined) {
+    status.progress = status.step || "DONE"
+
+    transcript.status = status
+
+    await database.updateTranscript(transcriptId, transcript)
+  }
+
+  // 3. Rename results and fields
+  const results = await database.getResults(transcriptId)
+
+  for (const [resultId, result] of Object.entries(results)) {
+    result.words = result.words.map(word => {
+      return {
+        ...word,
+        text: word.word,
+      }
+    })
+
+    await database.setParagraph(transcriptId, resultId, result)
+  }
+
+  response.sendStatus(200)
+}
+
+async function migrateAll(request: functions.Request, response: functions.Response) {
   // 1. Get all transcript ids
 
   console.log("transcripts")
