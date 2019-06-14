df71d700945828200b107412734e9e708e1f04d6
Bard Lind <bard.lind@nrk.no>
2019-06-14 08:32:08 +0200

    #6 introduced endpoint for the chron job

--- appengine/src/index.ts
+++ appengine/src/index.ts
@@ -1,8 +1,25 @@
+import serializeError = require("serialize-error");
+
 const PORT = Number(process.env.PORT) || 8080;
 import * as express from "express";
+import {findTransciptUpdatedTodayNotDone} from "./transcription/transcritpions";
 
 const app = express();
 
+app.get("/admin/transcripts/update", async (req, res) => {
+  console.log("run cron job")
+  try {
+    const transcripts = await findTransciptUpdatedTodayNotDone();
+    res.status(200).send("updated " + transcripts)
+  } catch (error) {
+    console.error("Failed to fetch transcripts. Error: ", error);
+
+    // Log error to Google Analytics
+    // visitor.exception(error.message, true).send()
+
+    res.status(500).send(serializeError(error))
+  }
+})
 app.get("/", (req, res) => {
   res.send("🎉 Hello TypeScript! 🎉");
 });
---
+++ appengine/src/transcription/transcritpions.ts
@@ -0,0 +1,6 @@
+
+export async function findTransciptUpdatedTodayNotDone(): Promise<string[]> {
+  const transcriptIds = []
+  transcriptIds.push("12345")
+  return transcriptIds
+}
