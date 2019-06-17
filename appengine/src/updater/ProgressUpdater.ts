90a0cda5e160484123925a1d8dff2d03ec9ad3b0
Bard Lind <bard.lind@nrk.no>
2019-06-17 08:17:20 +0200

    #6 Find out if transcript is eligeble for update.

--- appengine/src/updater/ProgressUpdater.ts
+++ appengine/src/updater/ProgressUpdater.ts
@@ -1,23 +1,43 @@
 import database from "../database";
+import {ITranscript} from "../interfaces";
+
 
 export class ProgressUpdater {
   private greeting: string;
+
   constructor(message: string) {
     this.greeting = message;
   }
+
   public greet(): string {
     return "Hello, " + this.greeting;
   }
 
   public async findTranscriptIdsEligibleForUpdate(): Promise<string[]> {
     const transcriptIds = []
-    const count = await database.findTransciptUpdatedTodayNotDone();
-    console.log("Eligable: ", JSON.stringify(count));
-    transcriptIds.push(JSON.stringify(count));
+    const transcripts = await database.findTransciptUpdatedTodayNotDone();
+    console.log("Eligable: ", JSON.stringify(transcripts));
+    if (transcripts) {
+      Object.values(transcripts).map((transcript) => {
+        if (this.isTranscriptProcessing(transcript)) {
+          if (this.hasTranscriptStoppedProgressing(transcript)) {
+            transcriptIds.push(transcript.id);
+          }
+        }
+      })
+    }
     return transcriptIds
   }
 
   public async update(): Promise<string[]> {
     return this.findTranscriptIdsEligibleForUpdate();
+  }
+
+  protected hasTranscriptStoppedProgressing(transcript: ITranscript) {
+    return false;
+  }
+
+  protected isTranscriptProcessing(transcript: ITranscript) {
+    return true;
   }
 }
